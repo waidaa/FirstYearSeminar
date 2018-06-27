a4d040c481368f0d7e517c22d57cf65aa084ed11
waidaa <40625868+waidaa@users.noreply.github.com>
2018-06-27 17:30:06 +0900

    Create force.py

---
+++ force.py
@@ -0,0 +1,60 @@
+from vpython import *
+import numpy as np
+
+#constants
+eps = 0.997
+sig = 3.30 * 10**(-10)
+delta = 2 * 10**(-10)
+dx = np.array([delta, 0, 0])
+dy = np.array([0, delta, 0])
+dz = np.array([0, 0, delta])
+
+class  atom:
+    def __init__(self, pos, mom, neighbours):
+        self.pos = pos #position
+        self.mom = mom #momentum
+        self.nei = neighbours
+
+def dis(x1, x2):
+    d = x1 - x2
+    return np.linalg.norm(d) 
+
+def ljpot(pos1, pos2):
+    dis = dis(pos1, pos2)
+    return (4 * eps) * (((sig**12)/(dis**12))-((sig**6)/(dis**6)))
+
+def force(index):
+    #constants
+    neilist = atoms[index].nei
+    pos_ = atoms[index].pos
+    posX = pos_ + dx
+    posY = pos_ + dy
+    posZ = pos_ + dz
+
+    def ljpot_(index):
+        pot_ = 0
+        for i in neilist:
+            pot_ += ljpot(pos_, atoms[i].pos)
+        return pot_
+    def ljpotX(index):
+        potX = 0
+        for i in neilist:
+            potX += ljpot(posX, atoms[i].pos)
+        return potX
+    def ljpotY(index):
+        potY = 0
+        for i in neilist:
+            potY += ljpot(posY, atoms[i].pos)
+        return potY
+    def ljpotZ(index):
+        potZ = 0
+        for i in neilist:
+            potZ += ljpot(posZ, atoms[i].pos)
+        return potZ
+    
+    fx = (ljpotX(index) - ljpot_(index))/delta
+    fy = (ljpotY(index) - ljpot_(index))/delta
+    fz = (ljpotZ(index) - ljpot_(index))/delta
+
+    return np.array([fx,fy,fz])
+#kokomade
